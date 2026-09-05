fbe270a7232c94181d13657ad6a67533e19ebaad
danavykhovanets-hub <dana.vykhovanets@gmail.com>
2026-09-05 17:56:02 -0400

    API + types

---
+++ lib/api.ts
@@ -0,0 +1,51 @@
+import axios from "axios";
+import type { Note, NoteTag } from "../types/note";
+
+interface FetchNotesResponse {
+  notes: Note[];
+  totalPages: number;
+}
+
+interface CreateNoteData {
+  title: string;
+  content: string;
+  tag: NoteTag;
+}
+
+const BASE_URL = "https://notehub-public.goit.study/api/notes";
+
+const authHeaders = {
+  Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
+};
+
+export async function fetchNotes(
+  search: string,
+  page: number
+): Promise<FetchNotesResponse> {
+  const response = await axios.get<FetchNotesResponse>(BASE_URL, {
+    params: {
+      search,
+      page,
+      perPage: 12,
+    },
+    headers: authHeaders,
+  });
+
+  return response.data;
+}
+
+export async function createNote(noteData: CreateNoteData): Promise<Note> {
+  const response = await axios.post<Note>(BASE_URL, noteData, {
+    headers: authHeaders,
+  });
+
+  return response.data;
+}
+
+export async function deleteNote(noteId: string): Promise<Note> {
+  const response = await axios.delete<Note>(`${BASE_URL}/${noteId}`, {
+    headers: authHeaders,
+  });
+
+  return response.data;
+}
